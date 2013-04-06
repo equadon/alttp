f753dca0e82191959001d8e11ed5969ec8009a20
Valekhz <valekhz@gmail.com>
2013-04-06 16:39:36 +0200

    Draw frame.

--- code/Alttp.Core/Graphics/Frame.cs
+++ code/Alttp.Core/Graphics/Frame.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Nuclex.Ninject.Xna;
 
 namespace Alttp.Core.Graphics
 {
@@ -15,6 +17,23 @@ namespace Alttp.Core.Graphics
         {
             Index = index;
             SpriteRefs = spriteRefs;
+        }
+
+        /// <summary>
+        /// Draw sprites in this frame.
+        /// </summary>
+        /// <param name="batch">SpriteBatch to draw with</param>
+        /// <param name="position">World position of frame</param>
+        public void Draw(ISpriteBatch batch, Vector2 position)
+        {
+            foreach (var spriteRef in SpriteRefs)
+            {
+                Vector2 spritePos = position;
+                spritePos.X += spriteRef.X;
+                spritePos.Y += spriteRef.Y;
+
+                batch.Draw(spriteRef.Sprite.Texture, spritePos, spriteRef.Sprite.Source, Color.White, spriteRef.Rotation, spriteRef.Sprite.Origin, 1, spriteRef.SpriteEffects, 0);
+            }
         }
     }
 }
