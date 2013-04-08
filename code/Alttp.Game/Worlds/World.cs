3f2518123dda4d24ce384843e6d9a9428d023c14
Valekhz <valekhz@gmail.com>
2013-04-08 21:48:51 +0200

    Do not store a reference to the Map object as we won't be needing it.

--- code/Alttp.Game/Worlds/World.cs
+++ code/Alttp.Game/Worlds/World.cs
@@ -9,20 +9,26 @@ namespace Alttp.Worlds
 {
     public class World : IWorld
     {
-        private readonly Map _map;
-
         public bool RenderCollisionTiles { get; set; }
 
         public Region[] Regions { get; private set; }
 
-        public int TileWidth { get { return _map.TileWidth; } }
-        public int TileHeight { get { return _map.TileWidth; } }
+        public int TileWidth { get; private set; }
+        public int TileHeight { get; private set; }
 
-        public int Width { get { return _map.Width; } }
-        public int Height { get { return _map.Height; } }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
 
         public int WidthInPixels { get { return Width * TileWidth; } }
         public int HeightInPixels { get { return Height * TileHeight; } }
+
+        public Tile[] SourceTiles { get; private set; }
+        public Tileset[] Tilesets { get; private set; }
+
+        // Layers
+        public TileLayer BackgroundTiles { get; private set; }
+        public TileLayer ForegroundTiles { get; private set; }
+        public TileLayer CollisionTiles { get; private set; }
 
         public World(IContentManager content, string mapResource)
             : this(content.Load<Map>(mapResource))
@@ -31,25 +37,56 @@ namespace Alttp.Worlds
 
         public World(Map map)
         {
-            _map = map;
+            TileWidth = map.TileWidth;
+            TileHeight = map.TileHeight;
+
+            Width = map.Width;
+            Height = map.Height;
+
+            // Layers
+            LoadTileLayers(map);
+
+            // Source tiles & tilesets
+            SourceTiles = map.SourceTiles;
+            Tilesets = map.Tilesets;
 
             // Load objects
             ObjectLayer regions = null;
+            ObjectLayer bushes = null;
 
-            foreach (var layer in _map.ObjectLayers)
+            // Object layers
+            foreach (var layer in map.ObjectLayers)
             {
                 if (layer.Name == "Regions")
                     regions = layer;
+                else if (layer.Name == "Bushes")
+                    bushes = layer;
             }
 
-            if (regions == null)
-                Regions = new Region[0];
-            else
-                Regions = LoadRegions(regions);
+            Regions = LoadRegions(regions);
+        }
+
+        private void LoadTileLayers(Map map)
+        {
+            foreach (var layer in map.TileLayers)
+            {
+                if (layer.Name == "Background")
+                    BackgroundTiles = layer;
+                else if (layer.Name == "Foreground")
+                    ForegroundTiles = layer;
+                else if (layer.Name == "Collision")
+                    CollisionTiles = layer;
+            }
+
+            if (BackgroundTiles == null || ForegroundTiles == null || CollisionTiles == null)
+                throw new Exception("Failed to load map layers");
         }
 
         private Region[] LoadRegions(ObjectLayer layer)
         {
+            if (layer == null)
+                return new Region[0];
+
             var regions = new List<Region>();
 
             foreach (var obj in layer.MapObjects)
@@ -70,13 +107,13 @@ namespace Alttp.Worlds
 
         public void Draw(GameTime gameTime, ISpriteBatch spriteBatch, Camera camera)
         {
-            DrawLayer(gameTime, spriteBatch, 0, camera, 0);
+            DrawLayer(gameTime, spriteBatch, BackgroundTiles, camera, 0);
 
             if (RenderCollisionTiles)
-                DrawLayer(gameTime, spriteBatch, 2, camera, 0);
+                DrawLayer(gameTime, spriteBatch, CollisionTiles, camera, 0);
         }
 
-        public void DrawLayer(GameTime gameTime, ISpriteBatch spriteBatch, int layerId, Camera camera, Single layerDepth)
+        public void DrawLayer(GameTime gameTime, ISpriteBatch spriteBatch, TileLayer layer, Camera camera, Single layerDepth)
         {
             Rectangle region = camera.GetTilesRegion();
 
@@ -85,31 +122,31 @@ namespace Alttp.Worlds
                 for (int x = region.Left; x <= region.Right; x++)
                 {
                     // check that we aren't going outside the map, and that there is a tile at this location
-                    if (x >= 0 && x < _map.TileLayers[layerId].Tiles.Length && y >= 0 && y < _map.TileLayers[layerId].Tiles[x].Length
-                        && _map.TileLayers[layerId].Tiles[x][y] != null)
+                    if (x >= 0 && x < layer.Tiles.Length && y >= 0 && y < layer.Tiles[x].Length
+                        && layer.Tiles[x][y] != null)
                     {
                         spriteBatch.Draw(
                             // the texture (image) of the tile sheet is mapped by
                             // Tile.SourceID -> TileLayers.TilesetID -> Map.Tileset.Texture
-                            _map.Tilesets[_map.SourceTiles[_map.TileLayers[layerId].Tiles[x][y].SourceID].TilesetID].Texture,
+                            Tilesets[SourceTiles[layer.Tiles[x][y].SourceID].TilesetID].Texture,
 
                             // destination for the tile
-                            new Rectangle(x * _map.TileWidth, y * _map.TileHeight, _map.TileWidth, _map.TileHeight),
+                            new Rectangle(x * TileWidth, y * TileHeight, TileWidth, TileHeight),
 
                             // source of the tile in the tilesheet
-                            _map.SourceTiles[_map.TileLayers[layerId].Tiles[x][y].SourceID].Source,
+                            SourceTiles[layer.Tiles[x][y].SourceID].Source,
 
                             // layers can have an opacity value, this property is Color.White at the opacity of the layer
-                            _map.TileLayers[layerId].OpacityColor,
+                            layer.OpacityColor,
 
                             // tile rotation value
-                            _map.TileLayers[layerId].Tiles[x][y].Rotation,
+                            layer.Tiles[x][y].Rotation,
 
                             // origin of the tile, this is always the center of the tile
-                            _map.SourceTiles[_map.TileLayers[layerId].Tiles[x][y].SourceID].Origin,
+                            SourceTiles[layer.Tiles[x][y].SourceID].Origin,
 
                             // tile horizontal or vertical flipping value
-                            _map.TileLayers[layerId].Tiles[x][y].Effects,
+                            layer.Tiles[x][y].Effects,
 
                             // depth for SpriteSortMode
                             layerDepth);
