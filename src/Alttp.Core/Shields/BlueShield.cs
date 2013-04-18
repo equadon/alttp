4d64d2c9980488bb1ff144c75cfb0269ee1af1ff
Valekhz <valekhz@gmail.com>
2013-04-18 13:13:21 +0200

    BlueShield class.

---
+++ src/Alttp.Core/Shields/BlueShield.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alttp.Core.GameObjects.Interfaces;
+
+namespace Alttp.Core.Shields
+{
+    public class BlueShield : Shield
+    {
+        public BlueShield(IGameObject parent)
+            : base(parent, ShieldType.Blue)
+        {
+        }
+    }
+}
